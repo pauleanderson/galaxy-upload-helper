570cae7b00f1b3a4eb975dc6c370a90d73d38212
Paul Anderson <andersonpe2@cofc.edu>
2014-08-06 13:40:01 -0400

    Initial creation of the scripts

---
+++ fix_create_dir.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import os
+
+email_file_lookup = '/etc/galaxy-upload-helper/emails.txt'
+galaxy_upload_dir = '/mnt/gv1/galaxy-upload'
+
+f = open(email_file_lookup,'r')
+lines = f.read().split("\n")
+for line in lines:
+        fields = line.split(" ")
+        if len(fields) >= 2:
+                username = fields[0]
+		email = fields[1]
+		os.system("mkdir "+galaxy_upload_dir+"/"+email)
+		os.system("chown -R "+username+":galaxy "+galaxy_upload_dir+"/"+email)
+		os.system("chmod -R gu+rwx "+galaxy_upload_dir+"/"+email)
+		os.system("chmod -R o-rwx "+galaxy_upload_dir+"/"+email)
+
---
+++ upload.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import getpass
+from os import listdir
+from os.path import isfile, join
+import sys
+import os
+
+email_file_lookup = '/etc/galaxy-upload-helper/emails.txt'
+galaxy_upload_dir = '/mnt/gv1/galaxy-upload'
+
+lookup = {}
+f = open(email_file_lookup,'r')
+lines = f.read().split("\n")
+for line in lines:
+	fields = line.split(" ")
+	if len(fields) >= 2:
+		lookup[fields[0]] = fields[1]
+
+username = getpass.getuser()
+
+if len(sys.argv) < 2:
+	print 'usage: upload.py <directory files to upload>'
+	exit(1)
+
+mypath = sys.argv[1]
+onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+for f in onlyfiles:
+	os.system("mv "+mypath+"/"+f+" "+galaxy_upload_dir+"/"+lookup[username]+"/")
