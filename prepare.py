b475db5255123f1dd1bd6e5da6331f165ca1e6e4
Paul Anderson <andersonpe2@cofc.edu>
2014-08-07 14:59:23 -0400

    Fixed prepare to better change permissions.

--- prepare.py
+++ prepare.py
@@ -4,17 +4,32 @@ filename = sys.argv[2]
 email = sys.argv[3]
 
 galaxy_upload_dir = '/mnt/gv1/galaxy-upload'
+email_file_lookup = '/etc/galaxy-upload-helper/emails.txt'
+
+lookup = {}
+f = open(email_file_lookup,'r')
+lines = f.read().split("\n")
+for line in lines:
+        fields = line.split(" ")
+        if len(fields) >= 2:
+                lookup[fields[1]] = fields[0]
 
 cmd = ["cp"] + [dataset] + [galaxy_upload_dir+"/"+email+"/download/"+filename]
 result = subprocess.Popen( cmd, stdout=subprocess.PIPE)
 
 stdout, stderr = result.communicate()
-stdout += "Command:\n"
-stdout += " ".join(cmd)
+if stdout != None:
+  print stdout
+if stderr != None:
+  print >> sys.stderr, stderr
 
-print "Standard out: "
-print stdout
-print "Standard error: "
-print stderr
+cmd = ["chown"] + [lookup[email]] + [galaxy_upload_dir+"/"+email+"/download/"+filename]
+result = subprocess.Popen( cmd, stdout=subprocess.PIPE)
+stdout, stderr = result.communicate()
+
+if stdout != None:
+  print stdout
+if stderr != None:
+  print >> sys.stderr, stderr
 
 os.system("touch completed")
